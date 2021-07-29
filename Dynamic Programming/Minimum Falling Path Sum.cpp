db3f2addaee0678d25965e9096b28cb1b8af0a08
Raj-292 <rajdpatel2000@gmail.com>
2021-07-29 12:12:58 +0530

    Minimum Falling Path Sum - Bottom Up

--- Dynamic Programming/Minimum Falling Path Sum.cpp
+++ Dynamic Programming/Minimum Falling Path Sum.cpp
@@ -1,3 +1,5 @@
+/*
+
 class Solution {
 public:
 
@@ -183,3 +185,200 @@ public:
         return res;
     }
 };
+
+*/
+
+// Attempt on bottom up cleaner approach (Using precomputation)
+
+
+
+class Solution {
+
+public:
+
+
+    //int helper(vector<vector<int>>& matrix, )
+
+
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+
+        int n = matrix.size();
+
+        vector<vector<int>> dp (n + 1, vector<int>(n + 2, -1));
+
+        cout << dp.size() << " " << dp[0].size() << endl;
+
+        // intialize last row with zeroes
+
+        for (int i = 0 ; i < dp[0].size() ; i++) {
+            dp[n][i] = 0;
+        }
+
+        // initialize first & last row with INT_MAX
+
+        // for (int i = 0 ; i < dp.size() ; i++) {
+        //     dp[0][i] = INT_MAX;
+        //     dp[n + 1][i] = INT_MAX;
+        // }
+
+        cout << dp.size() << " " << dp[0].size() << endl;
+
+        // for(int i = 0 ; i < dp.size() ; i++) {
+        //     for (int j = 0 ; j < dp[0].size() ; j++) {
+        //         cout<<dp[i][j]<<"   ";
+        //         if(j==dp[0].size()-1)
+        //             cout<<'\n';
+        //     }
+        // }
+
+
+        // for (int i = n - 1 ; i >= 0 ; i-- ) {
+        //     for (int j = 1 ; j <= n ; j++) {
+        //         dp[i][j] = matrix[i][j-1] + min({dp[i+1][j-1],dp[i+1][j],dp[i+1][j+1]});
+        //     }
+        // }
+
+
+        for (int i = 0 ; i < dp.size() ; i++) {
+            for (int j = 0 ; j < dp[0].size() ; j++) {
+                cout << dp[i][j] << "   ";
+                if (j == dp[0].size() - 1)
+                    cout << '\n';
+            }
+        }
+
+
+
+        return 0;
+
+    }
+};
+
+/* Some error in INT_MAX block ==> {
+
+    for (int i = 0 ; i < dp.size() ; i++) {
+            dp[i][0] = INT_MAX;
+            dp[i][n+1] = INT_MAX;
+        }
+}
+
+    This is the corrected initialization
+*/
+
+
+/* Working :
+
+class Solution {
+
+public:
+
+
+    //int helper(vector<vector<int>>& matrix, )
+
+
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+
+        int n = matrix.size();
+
+        vector<vector<int>> dp (n + 1, vector<int>(n + 2, -1));
+
+        cout << dp.size() << " " << dp[0].size() << endl;
+
+        // intialize last row with zeroes
+
+        for (int i = 0 ; i < dp[0].size() ; i++) {
+            dp[n][i] = 0;
+        }
+
+        // initialize first & last row with INT_MAX
+
+        for (int i = 0 ; i < dp.size() ; i++) {
+            dp[i][0] = INT_MAX;
+            dp[i][n + 1] = INT_MAX;
+        }
+
+        cout << dp.size() << " " << dp[0].size() << endl;
+
+        for (int i = 0 ; i < dp.size() ; i++) {
+            for (int j = 0 ; j < dp[0].size() ; j++) {
+                cout << dp[i][j] << "   ";
+                if (j == dp[0].size() - 1)
+                    cout << '\n';
+            }
+        }
+
+
+        for (int i = n - 1 ; i >= 0 ; i-- ) {
+            for (int j = 1 ; j <= n ; j++) {
+                dp[i][j] = matrix[i][j - 1] + min({dp[i + 1][j - 1], dp[i + 1][j], dp[i + 1][j + 1]});
+            }
+        }
+
+
+        for (int i = 0 ; i < dp.size() ; i++) {
+            for (int j = 0 ; j < dp[0].size() ; j++) {
+                cout << dp[i][j] << "   ";
+                if (j == dp[0].size() - 1)
+                    cout << '\n';
+            }
+        }
+
+        int res = INT_MAX;
+
+        for (int i = 0 ; i < dp[0].size() - 1 ; i++) {
+            res = min (res, dp[0][i]);
+        }
+
+
+
+        return res;
+
+    }
+};
+
+*/
+
+// Final Cleaned up :
+
+class Solution {
+
+public:
+
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+
+        int n = matrix.size();
+
+        vector<vector<int>> dp (n + 1, vector<int>(n + 2, -1));
+
+        // intialize last row with zeroes
+        for (int i = 0 ; i < n + 2 ; i++) {
+            dp[n][i] = 0;
+        }
+
+        // initialize first & last column with INT_MAX
+        for (int i = 0 ; i < n + 1 ; i++) {
+            dp[i][0] = INT_MAX;
+            dp[i][n + 1] = INT_MAX;
+        }
+
+        for (int i = n - 1 ; i >= 0 ; i-- ) {
+            for (int j = 1 ; j <= n ; j++) {
+                dp[i][j] = matrix[i][j - 1] + min({dp[i + 1][j - 1], dp[i + 1][j], dp[i + 1][j + 1]});
+            }
+        }
+
+        int res = INT_MAX;
+
+        for (int i = 1 ; i < n + 1 ; i++) {
+            res = min (res, dp[0][i]);
+        }
+
+        return res;
+    }
+};
+
+
+
+
+
+
